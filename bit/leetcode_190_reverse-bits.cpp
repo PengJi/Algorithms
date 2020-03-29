7b7181759eb5a92577b8783dc0ff75fcdc05b879
pengji <jipengpro@gmail.com>
2020-03-29 18:21:29 +0800

    add some files

---
+++ bit/leetcode_190_reverse-bits.cpp
@@ -0,0 +1,17 @@
+/**
+ * 190. 颠倒二进制位
+ * https://leetcode-cn.com/problems/reverse-bits/
+ */
+
+class Solution {
+public:
+    // 时间复杂度：O(1)
+    // 空间复杂度：O(1)
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t result= 0;
+        for(int i=0; i<32; i++)
+            result = (result << 1) + (n >> i & 1);
+        
+        return result;
+    }
+};
---
+++ sort/leetcode_242_valid-anagram.cpp
@@ -0,0 +1,33 @@
+/**
+ * 242. 有效的字母异位词
+ * https://leetcode-cn.com/problems/valid-anagram/
+ */
+
+class Solution {
+public:
+    // 排序
+    // 时间复杂度：O(nlogn)
+    // 空间复杂度：O(nlogn)
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+
+    // 哈希思想（推荐）
+    // 时间复杂度：O(n)
+    // 空间复杂度：O(1)
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        int count[26] = {0}
+        for(int i = 0; i < s.size(); i++){
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+        for(int i = 0; i<26; i++){
+            if(count[i]) return false;
+        }
+
+        return true;
+    }
+};
