04803f44264a8c487f7987962b5e504860c6fc10
pengji <jipengpro@gmail.com>
2020-04-29 08:49:33 +0800

    add 33 46

--- Backtracking/leetcode_46_permutations.cpp
+++ Backtracking/leetcode_46_permutations.cpp
@@ -6,6 +6,35 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> track;
+        backtrack(nums, track, res);
+        return res;
+    }
 
+    void backtrack(vector<int> nums, vector<int> track, vector<vector<int>> &res){
+        if(track.size() == nums.size()){
+            res.push_back(track);
+            return;
+        }
+
+        bool flag = false;
+        for(int i = 0; i < nums.size(); i++){
+            // 判断元素是否在路径中
+            for(int j = 0; j < track.size(); j++){
+                if(track[j] == nums[i]){
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag){
+                flag = false;
+                continue;
+            }
+
+            track.push_back(nums[i]);
+            backtrack(nums, track, res);
+            track.pop_back();
+        }
     }
 };
---
+++ search/leetcode_33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,33 @@
+/**
+ * 33. 搜索旋转排序数组
+ * https://leetcode-cn.com/problems/search-in-rotated-sorted-array/
+ */
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int mid;
+        int left = 0, right = nums.size()-1;
+        while(left <= right){
+            mid = left + (right - left) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[left] <= nums[mid]){  // 左边递增
+                if(nums[left] <= target && target < nums[mid]){
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {  // 右边递增
+                if(nums[mid] < target && target <= nums[right]){
+                    left = mid + 1;
+                } else {
+                    right = mid -1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
