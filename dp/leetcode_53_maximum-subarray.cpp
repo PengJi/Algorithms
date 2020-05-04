cec0d8a1f5878d5f9f349df0721633b15bbef304
pengji <jipengpro@gmail.com>
2020-05-04 09:45:52 +0800

    add 53

---
+++ dp/leetcode_53_maximum-subarray.cpp
@@ -0,0 +1,25 @@
+/**
+ * 53. 最大子序和
+ * https://leetcode-cn.com/problems/maximum-subarray/
+ */
+
+class Solution {
+public:
+    // 动态规划
+    // 时间复杂度：O(n)
+    // 空间复杂度：O(n)
+    int maxSubArray(vector<int>& nums) {
+        int len = nums.size();
+        if(len == 0) return 0;
+        if(len == 1) return nums[0];
+        int res = nums[0];
+        vector<int> dp(len);
+        dp[0] = nums[0];
+        for(int i = 1; i < len; i++){
+            dp[i] = max(dp[i-1] + nums[i], nums[i]);
+            res = max(res, dp[i]);
+        }
+
+        return res;
+    }
+};
