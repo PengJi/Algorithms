f0ceffb35a593da11250049ea1f19e64ce1dee4f
pengji <jipengpro@gmail.com>
2020-05-04 08:58:27 +0800

    add 3

---
+++ string/leetcode_3_longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,36 @@
+/**
+ * 3. 无重复字符的最长子串
+ * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+ */
+
+class Solution {
+public:
+    // 暴力法（左右指针）
+    // 时间复杂度：O(n^2)
+    // 空间复杂度：O(1)
+    int lengthOfLongestSubstring(string s) {
+        int length = s.size();
+        if(length == 0) return 0;
+        int left = 0, right=0, next = 1, max = 1;
+        while(right < length && next < length){
+            for(int i = left; i <= right; i++){
+                if(s[i] == s[next]){
+                    left = i+1;
+                }
+            }
+            if(left > right){
+                right = left;
+                next++;
+            }else{
+                right = next;
+                next++;
+            }
+        
+            if((right-left + 1) > max){
+                max = right - left + 1;
+            }
+        }
+
+        return max;
+    }
+};
