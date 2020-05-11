41bb2fe9e8a0e90a177e3e516d31b3fc6fbd4c8d
pengji <jipengpro@gmail.com>
2020-05-11 08:10:51 +0800

    modify 1143

--- dp/leetcode_1143_longest-common-subsequence.cpp
+++ dp/leetcode_1143_longest-common-subsequence.cpp
@@ -5,16 +5,27 @@
 
 class Solution {
 public:
+    // 动态规划
+    // 时间复杂度：O(mn)
+    // 空间复杂度：O(mn)
     int longestCommonSubsequence(string text1, string text2) {
-        if(text1.size() == 0 || text2.size() == 0)
+        int len1 = text1.size();
+        int len2 = text2.size();
+        if(len1 == 0 || len2 == 0){
             return 0;
-        vector<vector<int>> memo = vector(text1.size() + 1, vector(text2.size() + 1, 0)); // memo[i][j] 代表以 str1[i - 1]， str2[j - 1] 位置的元素为结尾的str1， str2 两个子字符串的 最长公共子序列
-        for(int i = 0; i < text1.size(); i++)
-            for(int j = 0; j < text2.size(); j++)
-                if(text1[i] == text2[j])
-                    memo[i + 1][j + 1] = memo[i][j] + 1;
-                else
-                    memo[i + 1][j + 1] = max(memo[i][j + 1], memo[i + 1][j]);
-        return memo[text1.size()][text2.size()];
+        }
+
+        vector<vector<int>> dp(len1+1, vector<int>(len2+1, 0));
+        for(int i = 1; i < len1 + 1 ; i++){
+            for(int j = 1; j < len2 + 1; j++){
+                if(text1[i-1] == text2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[len1][len2];
     }
 };
