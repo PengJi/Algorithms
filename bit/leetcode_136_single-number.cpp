52f80a9f911102ec56013ecd411eb467eb086377
pengji <jipengpro@gmail.com>
2020-05-14 23:57:13 +0800

    add 136

---
+++ bit/leetcode_136_single-number.cpp
@@ -0,0 +1,19 @@
+/**
+ * 136. 只出现一次的数字
+ * https://leetcode-cn.com/problems/single-number/
+ */
+
+class Solution {
+public:
+    // 位运算
+    // 时间复杂度：O(n)
+    // 空间复杂度：O(1)
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for(auto n : nums){
+            res ^= n;
+        }
+
+        return res;
+    }
+};
