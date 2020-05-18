d1df6d6ca4d66d26c4231a5a2918cfc45fdf1a1f
pengji <jipengpro@gmail.com>
2020-05-18 23:48:50 +0800

    modify 84

--- stack_queue/leetcode_84_largest-rectangle-in-histogram.cpp
+++ stack_queue/leetcode_84_largest-rectangle-in-histogram.cpp
@@ -29,6 +29,26 @@ public:
         return max;
     }
 
+    // 单调栈
+    // 时间复杂度：O(n)
+    // 空间复杂度：O(n)
     int largestRectangleArea(vector<int>& heights) {
+        int ans = 0;
+        vector<int> st;
+        heights.insert(heights.begin(), 0);
+        heights.push_back(0);
+        for (int i = 0; i < heights.size(); i++)
+        {
+            while (!st.empty() && heights[st.back()] > heights[i])
+            {
+                int cur = st.back();
+                st.pop_back();
+                int left = st.back() + 1;
+                int right = i - 1;
+                ans = max(ans, (right - left + 1) * heights[cur]);
+            }
+            st.push_back(i);
+        }
+        return ans;
     }
 };
