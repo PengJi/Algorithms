4117e992e198df54a4edcaca611e4d746f11bd34
pengji <jipengpro@gmail.com>
2020-05-25 23:55:55 +0800

    add 24

---
+++ list/leetcode_24_swap-nodes-in-pairs.cpp
@@ -0,0 +1,20 @@
+/**
+ * 24. 两两交换链表中的节点
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+ */
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+       
+    }
+};
