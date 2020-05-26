5300fb88faaa3b9dda4fc0a23b081728aec78a55
pengji <jipengpro@gmail.com>
2020-05-26 23:57:44 +0800

    add 24

--- list/leetcode_24_swap-nodes-in-pairs.cpp
+++ list/leetcode_24_swap-nodes-in-pairs.cpp
@@ -14,7 +14,53 @@
  */
 class Solution {
 public:
+    // 递归
+    // 时间复杂度：O(n)
+    // 空闲复杂度：O(n)
     ListNode* swapPairs(ListNode* head) {
+        // 如果有一个节点或者没有节点
+        if((head == NULL) || (head->next == NULL)){
+            return head;
+        }
+
+        // 待反转的节点
+        ListNode* start = head;
+        ListNode* end = head->next;
+
+        // 反转
+        start->next = swapPairs(end->next);
+        end->next = start;
+
+        return end;
+
+    }
+
+
+    // 迭代
+    // 时间复杂度：O(n)
+    // 空间复杂度：O(1)
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+
+        ListNode* prev = dummy;
+        while((head != NULL) && (head->next != NULL)){
+            // 待反转的节点
+            ListNode* start = head;
+            ListNode* end = head->next;
+
+            // 反转
+            prev->next = end;
+            start->next = end->next;
+            end->next = start;
+
+            // 初始化下一个反转的节点
+            prev = start;
+            head = start->next;  // 跳到下一个节点
+        }
+
+        return dummy->next;
        
     }
+
 };
