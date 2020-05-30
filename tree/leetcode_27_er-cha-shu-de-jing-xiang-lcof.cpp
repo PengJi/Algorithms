c812f62617a1ae9ae0e82c8e862070cbe520977b
pengji <jipengpro@gmail.com>
2020-05-30 23:00:37 +0800

    add interview 27

---
+++ tree/leetcode_27_er-cha-shu-de-jing-xiang-lcof.cpp
@@ -0,0 +1,24 @@
+/**
+ * 面试题27. 二叉树的镜像
+ * https://leetcode-cn.com/problems/er-cha-shu-de-jing-xiang-lcof/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mirrorTree(TreeNode* root) {
+        if(root == NULL) return NULL;
+        TreeNode* tmp = root->left;
+        root->left = mirrorTree(root->right);
+        root->right = mirrorTree(tmp);
+        return root;
+    }
+};
