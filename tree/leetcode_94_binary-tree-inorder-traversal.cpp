91defe890e1d25f441b72d8e4fecffa478fd6b72
pengji <jipengpro@gmail.com>
2020-05-31 17:20:30 +0800

    add 94

---
+++ tree/leetcode_94_binary-tree-inorder-traversal.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    // 递归
+    // 时间复杂度：O(n)
+    // 空间复杂度：O(n)
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        traverse(root, res);
+        return res;
+    }
+
+    void traverse(TreeNode* root, vector<int> &vec){
+        if(root != NULL){
+            traverse(root->left, vec);
+            vec.push_back(root->val);  // 中序遍历
+            traverse(root->right, vec);
+        }
+    }
+
+    // 迭代
+    // 时间复杂度：O(n)
+    // 空间复杂度：O(n)
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> stk;  // 利用栈
+        TreeNode* cur = root;
+
+        while(!stk.empty() || cur != NULL) {
+            while(cur != NULL){
+                stk.push(cur);
+                cur = cur->left;
+            }
+            cur = stk.top();
+            res.push_back(cur->val);
+            stk.pop();
+
+            cur = cur->right;
+        }
+
+        return res;
+    }
+};
