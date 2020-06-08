e09d8eeca085bc17a06d571c4a44fd8fc8aeaa23
pengji <jipengpro@gmail.com>
2020-06-09 00:09:47 +0800

    add 111

---
+++ tree/leetcode_111_minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * 111. 二叉树的最小深度
+ * https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    // 递归
+    // 时间复杂度：O(n)
+    // 空间复杂度：O(n)
+    int minDepth(TreeNode* root) {
+        if(root == nullptr) return 0;
+        int leftHeight = minDepth(root->left);
+        int rightHeight = minDepth(root->right);
+        if(!leftHeight || !rightHeight) return leftHeight+rightHeight+1;
+        return min(leftHeight, rightHeight) + 1;
+    }
+};
