4557ac7e226aba65511450f3774ff3feb3552066
pengji <jipengpro@gmail.com>
2020-06-09 23:57:21 +0800

    add 235

---
+++ tree/leetcode_235_lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * 235. 二叉搜索树的最近公共祖先
+ * https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    // 递归
+    // https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/solution/lca-by-ai-mao-de-xiao-gen-ban/
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        //递归边界返回空或者返回存在的那个结点
+        if(root==NULL||root==p||root==q) return root;
+        //左边找一找
+        TreeNode* L=lowestCommonAncestor(root->left,p,q);
+        //右边找一找
+        TreeNode* R=lowestCommonAncestor(root->right,p,q);
+        //返回NULL或者有的那一侧
+        if(L==NULL)return R;
+        if(R==NULL)return L;
+        //当前祖先左边有p，右边有q
+        return root; 
+    }
+};
