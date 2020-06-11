9ffb55a7ee1046379103798824fdef0f657df315
pengji <jipengpro@gmail.com>
2020-06-12 00:22:45 +0800

    modify 563

--- tree/leetcode_563_binary-tree-tilt.cpp
+++ tree/leetcode_563_binary-tree-tilt.cpp
@@ -1,4 +1,9 @@
 /**
+ * 563. 二叉树的坡度
+ * https://leetcode-cn.com/problems/binary-tree-tilt/
+ */
+
+/**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
@@ -9,16 +14,23 @@
  */
 class Solution {
 public:
+    // 递归
+    // 时间复杂度：O(n)
+    // 空间复杂度：O(n)
+    int res = 0;
     int findTilt(TreeNode* root) {
-        int res = 0;
-        traverse(root, res);
+        traverse(root);
         return res;
     }
 
-    void traverse(TreeNode* root, int &res){
-        if(root == nullptr) return;
-        int left = traverse(root->left, res);
-        int right = traverse(root->right, res);
+    int traverse(TreeNode* root){
+        if(root == nullptr){
+            return 0;
+        }
+
+        int left = traverse(root->left);
+        int right = traverse(root->right);
         res += abs(left - right);
+        return left + right + root->val;
     }
 };
