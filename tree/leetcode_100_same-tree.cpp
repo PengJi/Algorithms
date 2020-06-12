aa9c3f8b2cd120bf3c8a461be0d4c4d92ba432e4
pengji <jipengpro@gmail.com>
2020-06-12 22:26:55 +0800

    add 100

---
+++ tree/leetcode_100_same-tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * 100. 相同的树
+ * https://leetcode-cn.com/problems/same-tree/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    // 递归
+    // 时间复杂度：O(n)
+    // 空间复杂度：O(n)
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == nullptr && q == nullptr) return true;
+        if(p == nullptr || q == nullptr) return false;
+        if(p->val != q->val) return false;
+
+        return isSameTree(p->right, q->right) && isSameTree(p->left, q->left);
+    }
+};
