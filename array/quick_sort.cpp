57c8b71558266f1042db9ad259d818b7625438f5
pengji <jipengpro@gmail.com>
2020-06-19 08:26:03 +0800

    modify quick sort

--- array/quick_sort.cpp
+++ array/quick_sort.cpp
@@ -1,24 +1,47 @@
- Paritition1(int A[], int low, int high) {
-   int pivot = A[low];
-   while (low < high) {
-     while (low < high && A[high] >= pivot) {
-       --high;
-     }
-     A[low] = A[high];
-     while (low < high && A[low] <= pivot) {
-       ++low;
-     }
-     A[high] = A[low];
-   }
-   A[low] = pivot;
-   return low;
- }
+/**
+ * 快速排序
+ */
 
- void QuickSort(int A[], int low, int high) //快排母函数
- {
-   if (low < high) {
-     int pivot = Paritition1(A, low, high);
-     QuickSort(A, low, pivot - 1);
-     QuickSort(A, pivot + 1, high);
-   }
- }
+#include<stdio.h>
+
+int parittition(int arr[], int low, int high) {
+    int pivot = arr[low];
+
+    while(low < high) {
+        while(low < high && pivot <= arr[high]) {
+            --high;
+        }
+        arr[low] = arr[high];
+
+        while(low < high && pivot >= arr[low]) {
+            ++low;
+        }
+        arr[high] = arr[low];
+    }
+
+    arr[low] = pivot;
+    return low;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if(low < high) {
+        int pivot = parittition(arr, low, high);
+        quickSort(arr, low, pivot-1);
+        quickSort(arr, pivot+1, high);
+    }
+
+}
+
+int main() {
+    int arr[10] = {0, 5, 1, 9, 6, 8, 19, 11, 3, 7};
+    for(int i = 0;i < 10; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    quickSort(arr, 0, 9);
+
+    for(int i = 0;i < 10; i++){
+        printf("%d ",arr[i]);
+    }
+}
