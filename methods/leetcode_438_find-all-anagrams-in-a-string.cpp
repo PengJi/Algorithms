3a3a2f4132ac5592f0a0f59a83d2915d28c41967
pengji <jipengpro@gmail.com>
2020-06-20 18:55:32 +0800

    add methods

---
+++ methods/leetcode_438_find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,44 @@
+/**
+ * 438. 找到字符串中所有字母异位词
+ * https://leetcode-cn.com/problems/find-all-anagrams-in-a-string/
+ */
+
+class Solution {
+public:
+    // 滑动窗口
+    // 时间复杂度：O(n)
+    // 空间复杂度：O(n)
+    vector<int> findAnagrams(string s, string t) {
+        unordered_map<char, int> need, window;
+        for (char c : t) need[c]++;
+
+        int left = 0, right = 0;
+        int valid = 0;
+        vector<int> res; // 记录结果
+        while (right < s.size()) {
+            char c = s[right];
+            right++;
+            // 进行窗口内数据的一系列更新
+            if (need.count(c)) {
+                window[c]++;
+                if (window[c] == need[c]) 
+                    valid++;
+            }
+            // 判断左侧窗口是否要收缩
+            while (right - left >= t.size()) {
+                // 当窗口符合条件时，把起始索引加入 res
+                if (valid == need.size())
+                    res.push_back(left);
+                char d = s[left];
+                left++;
+                // 进行窗口内数据的一系列更新
+                if (need.count(d)) {
+                    if (window[d] == need[d])
+                        valid--;
+                    window[d]--;
+                }
+            }
+        }
+        return res;
+    }
+};
