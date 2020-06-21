b7c8c44a9959b0826d03847de5b8e4348db446d5
pengji <jipengpro@gmail.com>
2020-06-21 22:23:34 +0800

    add 518

--- dp/leetcode_518_coin-change-2.cpp
+++ dp/leetcode_518_coin-change-2.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
+    // 递归
+    // 时间复杂度：O(amount * coins.size())
+    // 空间复杂度：O(amount)
     int change(int amount, vector<int>& coins) {
-        
+        int dp[amount+1];
+        memset(dp, 0, sizeof(dp)); //初始化数组为0
+        dp[0] = 1;
+        for (int coin : coins){ //枚举硬币
+            for (int i = 1; i <= amount; i++){ //枚举金额
+                if (i < coin) continue; // coin不能大于amount
+                dp[i] += dp[i-coin];
+            }
+        }
+        return dp[amount];
     }
 };
