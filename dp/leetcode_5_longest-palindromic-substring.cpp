09b81e100bbed6526b6ef8b7223b39819d5a8afc
pengji <jipengpro@gmail.com>
2020-07-04 22:41:34 +0800

    modify 5

--- dp/leetcode_5_longest-palindromic-substring.cpp
+++ dp/leetcode_5_longest-palindromic-substring.cpp
@@ -6,9 +6,34 @@
 class Solution {
 public:
     // 动态规划
+    // 时间复杂度：O(n^2)
+    // 空间复杂度：O(n^2)
     string longestPalindrome(string s) {
-        if(s.size() <= 1)
-            return s;
+        int len = s.size();
+        if(len <= 1) return s;
+
+        int maxLen = 1;
+        string ans = s.substr(0, 1);
+        vector<vector<bool>> dp(len, vector<bool>(len, false));
+        for(int i = len-1; i >= 0; i--) {
+            for(int j = i; j < len; j++) {
+                dp[i][j] = s[i] == s[j] && (i+1 > j-1 || dp[i+1][j-1]);
+                if(dp[i][j] && j-i+1 > maxLen) {
+                    maxLen = j-i+1;
+                    ans = s.substr(i, maxLen);
+                }
+            }
+        }
+
+        return ans;
+    } 
+
+
+    // 动态规划
+    // 时间复杂度：O(n^2)
+    // 空间复杂度：O(n^2)
+    string longestPalindrome(string s) {
+        if(s.size() <= 1) return s;
 
         int start = 0,maxLen = 1, n = s.size();
         bool isPal[1000][1000] = {false};
