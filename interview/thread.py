5dbf05f762649ddbb8f61bcaf3104bf03199389a
pengji <jipengpro@gmail.com>
2020-07-19 21:42:15 +0800

    add interview

--- interview/thread.py
+++ interview/thread.py
@@ -4,11 +4,16 @@ from Queue import Queue
 from threading import Thread
 import time
 
+
 # 实现自定义线程池
 
 queue = Queue()
 
+
 class ThreadNum(Thread):
+    """
+    创建线程
+    """
     def __init__(self, queue):
         Thread.__init__(self)
         self.queue = queue
@@ -17,14 +22,16 @@ class ThreadNum(Thread):
         while True:
             num = self.queue.get()
             print("retrieved ", num)
+
             time.sleep(1)
+
             self.queue.task_done()
 
         print("finished")
 
 
 def main():
-    # 创建一个 thread pool, 这里开启5个并发
+    # 创建一个 thread pool, 这里开启5个并发线程
     for i in range(5):
         t = ThreadNum(queue)
         t.setDaemon(True)
@@ -35,6 +42,7 @@ def main():
         queue.put(num)
 
     queue.join()
+
 
 if __name__ == '__main__':
     main()
