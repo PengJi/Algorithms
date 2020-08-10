ac9fc3a6ded348b140e75802eeead15471415602
pengji <jipengpro@gmail.com>
2020-08-10 23:52:53 +0800

    add 344

---
+++ string/leetcode_344_reverse-string.cpp
@@ -0,0 +1,22 @@
+/**
+ * 344. 反转字符串
+ * https://leetcode-cn.com/problems/reverse-string/
+ */
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        if(s.size() == 0) return;
+
+        int left = 0;
+        int right = s.size()-1;
+        char tmp;
+        while(left < right) {
+            tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+};
