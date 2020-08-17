77806a58dd3b3147e5b7e7bf92c171ed8bb88ace
pengji <jipengpro@gmail.com>
2020-08-17 23:57:15 +0800

    add 88

---
+++ array/leetcode_88_merge-sorted-array.cpp
@@ -0,0 +1,33 @@
+/**
+ * 88. 合并两个有序数组
+ * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+ * 
+ * https://leetcode-cn.com/problems/merge-sorted-array/
+ */
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int i = nums1.size() - 1;
+    m--;
+    n--;
+    while (n >= 0) {
+        while (m >= 0 && nums1[m] > nums2[n]) {
+            swap(nums1[i--], nums1[m--]);
+        }
+        swap(nums1[i--], nums2[n--]);
+    }
+    }
+};
+
+    // void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    //     int idx = m-1;
+    //     m--;
+    //     n--;
+    //     while(n >= 0) {
+    //         while(m >= 0 && nums1[m] > nums2[n]) {
+    //             swap(nums1[idx--], nums1[m--]);
+    //         }
+    //         swap(nums1[idx--], nums2[n--]);
+    //     }
+    // }
