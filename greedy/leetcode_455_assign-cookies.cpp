ff03bcba8ed61737bc87bab223ceb4f15672f691
pengji <jipengpro@gmail.com>
2020-08-21 23:17:03 +0800

    add 455

---
+++ greedy/leetcode_455_assign-cookies.cpp
@@ -0,0 +1,25 @@
+/**
+ * 455. 分发饼干
+ * https://leetcode-cn.com/problems/assign-cookies/
+ */
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int i = 0, j = 0;
+        int res = 0;
+        while(i < g.size() && j < s.size()) {
+            if(g[i] <= s[j]) {
+                res++;
+                i++;
+            }
+
+            j++;
+        }
+
+        return res;
+    }
+};
