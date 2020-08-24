f9c64ce68a24aec5d41ef536c736424f29591e52
pengji <jipengpro@gmail.com>
2020-08-24 21:38:58 +0800

    modify files

--- DFS_BFS/LeetCode_200_number-of-islands.cpp
+++ DFS_BFS/LeetCode_200_number-of-islands.cpp
@@ -24,7 +24,7 @@ public:
         for(int i = 0; i < row; i++) {
             for(int j = 0; j < col; j++) {
                 if(grid[i][j] == '1') {
-                    removeIsland(grid, i, j);
+                    removeIsland(grid, i, j);  // 修改值
                     ans++;
                 }
             }
--- DFS_BFS/leetcode_695_max-area-of-island.cpp
+++ DFS_BFS/leetcode_695_max-area-of-island.cpp
@@ -1,35 +1,46 @@
 /**
  * 695. 岛屿的最大面积
+ * 给定一个包含了一些 0 和 1 的非空二维数组 grid 。
+ * 一个 岛屿 是由一些相邻的 1 (代表土地) 构成的组合，
+ * 这里的「相邻」要求两个 1 必须在水平或者竖直方向上相邻。
+ * 你可以假设 grid 的四个边缘都被 0（代表水）包围着。
+ * 找到给定的二维数组中最大的岛屿面积。(如果没有岛屿，则返回面积为 0 。)
  * 
  * https://leetcode-cn.com/problems/max-area-of-island/
  */
 
 class Solution {
 public:
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int res = 0;
-        for(int i = 0; i < grid.size(); i++) 
-            for(int j = 0; j < grid[0].size(); j++)
-                res = max(res, dfs(grid, i, j));
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
 
-        return res;
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        if(grid.empty()) return 0;
+        int row = grid.size();
+        int col = grid[0].size();
+
+        int ans = 0;
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                ans = max(ans, dfs(grid, i, j));  // 选最大值
+            }
+        }
+
+        return ans;
     }
 
-private:
-    int ci[4] = {0, 0, 1, -1}, cj[4] = {1, -1, 0, 0};
     int dfs(vector<vector<int>>& grid, int i, int j) {
-        if(i < 0 || j < 0 || i == grid.size() || j == grid[0].size() || grid[i][j] != 1)
-            return 0;
+        if(grid[i][j] == 0) return 0;
 
         grid[i][j] = 0;  // 访问过的节点设置为0，避免重复访问
-
         int ans = 1;
-        int next_i, next_j;
-        // 遍历四个方向
-        for(int idx = 0; idx < 4; idx++) {
-            next_i = i + ci[idx];
-            next_j = j + cj[idx];
-            ans += dfs(grid, next_i, next_j);
+        for(int k = 0; k < 4; k++) {  // 遍历四个方向
+            int nx = i + dx[k];
+            int ny = j + dy[k];
+
+            if(nx >= 0 && ny >= 0 && nx < grid.size() && ny < grid[0].size()) {
+                ans += dfs(grid, nx, ny);
+            }
         }
 
         return ans;
