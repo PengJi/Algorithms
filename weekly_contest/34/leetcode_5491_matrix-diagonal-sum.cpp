070d0c8f42c0a1ce232771c0aa5fe07cd34ce2e6
pengji <jipengpro@gmail.com>
2020-09-06 23:34:28 +0800

    modify 5491

--- weekly_contest/34/leetcode_5491_matrix-diagonal-sum.cpp
+++ weekly_contest/34/leetcode_5491_matrix-diagonal-sum.cpp
@@ -1,3 +1,14 @@
 /**
  * 5491. 矩阵对角线元素的和
+ * 给你一个正方形矩阵 mat，请你返回矩阵对角线元素的和。
+ * 请你返回在矩阵主对角线上的元素和副对角线上且不在主对角线上元素的和。
+ * 
+ * https://leetcode-cn.com/problems/matrix-diagonal-sum/
  */
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+
+    }
+};
