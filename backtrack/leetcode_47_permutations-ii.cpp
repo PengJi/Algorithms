c48474d35a45a84ee357773ff785d1e73774c66d
pengji <jipengpro@gmail.com>
2020-09-30 22:12:32 +0800

    add 47

---
+++ backtrack/leetcode_47_permutations-ii.cpp
@@ -0,0 +1,38 @@
+/**
+ * 47. 全排列 II
+ * 给定一个可包含重复数字的序列，返回所有不重复的全排列。
+ * 
+ * https://leetcode-cn.com/problems/permutations-ii/
+ */
+
+class Solution {
+public:
+    vector<int> vis;
+    // 回溯法
+    // https://leetcode-cn.com/problems/permutations-ii/solution/quan-pai-lie-ii-by-leetcode-solution/
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> perm;
+        vis.resize(nums.size());
+        sort(nums.begin(), nums.end());
+        backtrack(nums, ans, 0, perm);
+        return ans;
+    }
+
+    void backtrack(vector<int>& nums, vector<vector<int>>& ans, int idx, vector<int>& perm) {
+        if (idx == nums.size()) {
+            ans.emplace_back(perm);
+            return;
+        }
+        for (int i = 0; i < (int)nums.size(); ++i) {
+            if (vis[i] || (i > 0 && nums[i] == nums[i - 1] && !vis[i - 1])) {
+                continue;
+            }
+            perm.emplace_back(nums[i]);
+            vis[i] = 1;
+            backtrack(nums, ans, idx + 1, perm);
+            vis[i] = 0;
+            perm.pop_back();
+        }
+    }
+};
