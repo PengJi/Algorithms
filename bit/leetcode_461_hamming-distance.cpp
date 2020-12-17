1d36660bab4916662418a8885708d3f37a18ef0f
pengji <jipengpro@gmail.com>
2020-12-17 23:05:04 +0800

    add 461

---
+++ bit/leetcode_461_hamming-distance.cpp
@@ -0,0 +1,19 @@
+/**
+ * 461. 汉明距离
+ * https://leetcode-cn.com/problems/hamming-distance/
+ */
+
+class Solution {
+public:
+    // https://leetcode-cn.com/problems/hamming-distance/solution/yi-ming-ju-chi-by-leetcode/
+    int hammingDistance(int x, int y) {
+        int tmp = x ^ y;
+        int distance = 0;
+        while (tmp != 0) {
+          distance += 1;
+          // remove the rightmost bit of '1'
+          tmp = tmp & (tmp - 1);
+        }
+        return distance;
+    }
+};
