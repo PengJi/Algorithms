c6058a2ff297a0597b0cb141a8731d2047bc780c
pengji <jipengpro@gmail.com>
2020-12-19 22:53:38 +0800

    add 160

---
+++ list/leetcode_160_intersection-of-two-linked-lists.cpp
@@ -0,0 +1,29 @@
+/**
+ * 160. 相交链表
+ * https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA == NULL || headB == NULL) {
+            return NULL;  
+        }
+
+        ListNode* pA = headA, pB = headB;
+        while(pa != pb) {
+            pA = pA == NULL ? headB : pA->next;
+            pB = pB == NULL ? headA : pB->next;
+        }
+
+        return pA;
+    }
+};
