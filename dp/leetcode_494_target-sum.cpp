8028ed5cbce492e63ba7293200334f12318d9e95
pengji <jipengpro@gmail.com>
2020-12-21 23:13:51 +0800

    add 494

---
+++ dp/leetcode_494_target-sum.cpp
@@ -0,0 +1,29 @@
+/**
+ * 494. 目标和
+ * https://leetcode-cn.com/problems/target-sum/
+ */
+
+class Solution {
+public:
+    // https://leetcode-cn.com/problems/target-sum/solution/c-dfshe-01bei-bao-by-bao-bao-ke-guai-liao/
+    int findTargetSumWays(vector<int>& nums, int S) {
+        long sum = 0;
+        for (const int &it : nums) {
+            sum += it;
+        }
+        if ((S + sum) % 2 == 1 || S > sum) {
+            return 0;
+        }
+        S = (S + sum) / 2;
+        int *dp = new int[S + 1];
+        memset(dp, 0, (S + 1) * sizeof(int));
+        dp[0] = 1;
+        for (const int &it : nums) {
+            for (int j = S; j >= it; j--)
+                dp[j] += dp[j - it];
+        }
+        int ans = dp[S];
+        delete[] dp;
+        return ans;
+    }
+};
