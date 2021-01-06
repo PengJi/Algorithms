f2c3cbc6fab687238d0e2235dee9a403289f9e03
pengji <jipengpro@gmail.com>
2021-01-06 23:06:17 +0800

    modify 238

--- array/leetcode_238_product-of-array-except-self.cpp
+++ array/leetcode_238_product-of-array-except-self.cpp
@@ -8,13 +8,13 @@ public:
     // https://leetcode-cn.com/problems/product-of-array-except-self/solution/chu-zi-shen-yi-wai-shu-zu-de-cheng-ji-by-leetcode-/
     vector<int> productExceptSelf(vector<int>& nums) {
         int length = nums.size();
-        vector<int> answer(length);
+        vector<int> ans(length);
 
         // answer[i] 表示索引 i 左侧所有元素的乘积
         // 因为索引为 '0' 的元素左侧没有元素， 所以 answer[0] = 1
-        answer[0] = 1;
+        ans[0] = 1;
         for (int i = 1; i < length; i++) {
-            answer[i] = nums[i - 1] * answer[i - 1];
+            ans[i] = nums[i - 1] * ans[i - 1];
         }
 
         // R 为右侧所有元素的乘积
@@ -22,10 +22,10 @@ public:
         int R = 1;
         for (int i = length - 1; i >= 0; i--) {
             // 对于索引 i，左边的乘积为 answer[i]，右边的乘积为 R
-            answer[i] = answer[i] * R;
+            ans[i] = ans[i] * R;
             // R 需要包含右边所有的乘积，所以计算下一个结果时需要将当前值乘到 R 上
             R *= nums[i];
         }
-        return answer;
+        return ans;
     }
 };
