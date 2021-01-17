70cfd9c5b3649f5277d4ff294f499d2227d4c8cc
pengji <jipengpro@gmail.com>
2021-01-17 23:28:03 +0800

    add 5655

---
+++ weekly_contest/224/leetcode_5655_largest-submatrix-with-rearrangements.cpp
@@ -0,0 +1,33 @@
+/**
+ * 5655. 重新排列后的最大子矩阵
+ * https://leetcode-cn.com/problems/largest-submatrix-with-rearrangements/
+ */
+
+class Solution {
+public:
+    int largestSubmatrix(vector<vector<int>>& matrix) {
+        int h = matrix.size(), w = matrix[0].size();
+        vector<vector<int>> up(h, vector<int>(w, 0));
+        
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                if (matrix[i][j] == 1) {
+                    up[i][j] = (i == 0 ? 0 : up[i-1][j]) + 1;
+                }
+            }
+        }
+
+        int ret = 0;
+        for (int i = 0; i < h; i++) {
+            vector<int> buf;
+            for (int j = 0; j < w; j++) {
+                buf.push_back(up[i][j]);
+            }
+            sort(buf.begin(), buf.end());
+            for (int j = 0; j < w; j++) {
+                ret = max(ret, buf[j] * (w - j));
+            }
+        }
+        return ret;
+    }
+};
