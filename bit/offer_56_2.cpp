9913593cb80bceb79998bb5a819ef23fc37ecf15
pengji <jipengpro@gmail.com>
2021-02-16 22:56:37 +0800

    add offer 56 2

--- bit/offer_56_2.cpp
+++ bit/offer_56_2.cpp
@@ -5,7 +5,14 @@
 
 class Solution {
 public:
+    // https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof/solution/mian-shi-ti-56-ii-shu-zu-zhong-shu-zi-chu-xian-d-4/
     int singleNumber(vector<int>& nums) {
+        int ones = 0. twos = 0;
+        for(int num: nums) {
+            ones = ones ^ num & ~twos;
+            twos = twos ^ num & ~ones;
+        }
 
+        return ones;
     }
 };
