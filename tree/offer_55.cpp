5a2447094aaa0bdb5ea794d65285b1f60c9a3490
pengji <jipengpro@gmail.com>
2021-02-20 21:24:44 +0800

    add offer 55

---
+++ tree/offer_55.cpp
@@ -0,0 +1,22 @@
+/**
+ * 剑指 Offer 55 - I. 二叉树的深度
+ * https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    // 递归
+    int maxDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
