d0ccb91fd9af4c3499e83ab709d8e6dfb349dffa
pengji <jipengpro@gmail.com>
2021-03-02 22:20:41 +0800

    modify offer 40

--- search/offer_40.cpp
+++ search/offer_40.cpp
@@ -46,5 +46,34 @@ public:
     // 时间复杂度：
     // 空间复杂度：
     vector<int> getLeastNumbers(vector<int>& arr, int k) {
+        if (k >= arr.size()) return arr;
+        return quickSort(arr, k, 0, arr.size() - 1);
+    }
+    
+    vector<int> quickSort(vector<int>& arr, int k, int l, int r) {
+        int i = l, j = r;
+        while (i < j) {
+            while (i < j && arr[j] >= arr[l]) j--;
+            while (i < j && arr[i] <= arr[l]) i++;
+            swap(arr[i], arr[j]);
+        }
+        swap(arr[i], arr[l]);
+        if (i > k) return quickSort(arr, k, l, i - 1);
+        if (i < k) return quickSort(arr, k, i + 1, r);
+        vector<int> res;
+        res.assign(arr.begin(), arr.begin() + k);
+        return res;
     }
 };
+
+
+    vector<int> getLeastNumbers(vector<int>& arr, int k) {
+
+    }
+private:
+
+
+作者：jyd
+链接：https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/solution/jian-zhi-offer-40-zui-xiao-de-k-ge-shu-j-9yze/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
