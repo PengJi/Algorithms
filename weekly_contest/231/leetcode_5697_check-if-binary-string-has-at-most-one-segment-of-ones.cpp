43e217617295f6d7042b454cfb2a58425b197542
pengji <jipengpro@gmail.com>
2021-03-07 17:32:17 +0800

    add 5697

---
+++ weekly_contest/231/leetcode_5697_check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -0,0 +1,16 @@
+/**
+ * 5697. 检查二进制字符串字段
+ * https://leetcode-cn.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/
+ */
+
+class Solution {
+public:
+    bool checkOnesSegment(string s) {
+        if(s.size() == 1)  return true;
+        for(int i = 1; i <= s.size()-1; i++) {
+            if(s[i-1] == '0' && s[i] == '1') return false;
+        }
+        
+        return true;
+    }
+};
