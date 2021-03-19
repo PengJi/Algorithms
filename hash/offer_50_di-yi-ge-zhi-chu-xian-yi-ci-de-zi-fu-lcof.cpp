788bcc7e47e8f914e1e1fe7629d198ec76496d5b
pengji <jipengpro@gmail.com>
2021-03-19 22:19:21 +0800

    add offer 50

---
+++ hash/offer_50_di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof.cpp
@@ -0,0 +1,21 @@
+/**
+ * 剑指 Offer 50. 第一个只出现一次的字符
+ * https://leetcode-cn.com/problems/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof/
+ */
+
+class Solution {
+public:
+    // https://leetcode-cn.com/problems/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof/solution/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-by-3zqv5/
+    char firstUniqChar(string s) {
+        unordered_map<int, int> frequency;
+        for (char ch: s) {
+            ++frequency[ch];
+        }
+        for (int i = 0; i < s.size(); ++i) {
+            if (frequency[s[i]] == 1) {
+                return s[i];
+            }
+        }
+        return ' ';
+    }
+};
