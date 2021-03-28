9f55a9415b18f9b0af90ffb2742a0047509fd681
pengji <jipengpro@gmail.com>
2021-03-28 23:11:49 +0800

    modify 5713

--- weekly_contest/234/leetcode_5713_number-of-different-integers-in-a-string.cpp
+++ weekly_contest/234/leetcode_5713_number-of-different-integers-in-a-string.cpp
@@ -6,6 +6,26 @@
 class Solution {
 public:
     int numDifferentIntegers(string word) {
-
+        set<string> st;
+        int idx = 0;
+        while(idx < word.size()) {
+            if(word[idx] >= '0' && word[idx] <= '9') {
+                int s = 1;
+                while(s <= word.size() && word[idx] >= '0' && word[idx] <= '9') {
+                    idx++;
+                    s++;
+                }
+                string tmp = word.substr(idx-s+1, s-1);
+                int b = 0;
+                while(tmp[b] == '0') {
+                    b++;
+                }
+                st.insert(tmp.substr(b));
+            } else {
+                idx++;
+            } 
+        }
+        
+        return st.size();
     }
 };
