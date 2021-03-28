689b4df7b65a5c3c72e29d5f4f3650633feef5ba
pengji <jipengpro@gmail.com>
2021-03-28 22:44:10 +0800

    add 5713

---
+++ weekly_contest/234/leetcode_5713_number-of-different-integers-in-a-string.cpp
@@ -0,0 +1,11 @@
+/**
+ * 5713. 字符串中不同整数的数目
+ * https://leetcode-cn.com/problems/number-of-different-integers-in-a-string/
+ */
+
+class Solution {
+public:
+    int numDifferentIntegers(string word) {
+
+    }
+};
