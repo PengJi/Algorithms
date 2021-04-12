37bdddf5ef58240fc02875ac07a32db32ed06f07
pengji <jipengpro@gmail.com>
2021-04-12 22:39:39 +0800

    add 162

---
+++ search/leetcode_162_find-peak-element.cpp
@@ -0,0 +1,18 @@
+/**
+ * 162. 寻找峰值
+ * https://leetcode-cn.com/problems/find-peak-element/
+ */
+
+class Solution {
+public:
+    // https://www.acwing.com/solution/content/258/
+    int findPeakElement(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (nums[mid] > nums[mid + 1]) r = mid;
+            else l = mid + 1;
+        }
+        return r;
+    }
+};
