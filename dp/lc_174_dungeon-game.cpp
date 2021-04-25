9007ef0535c5b3bb1213fbabff9110e33f088443
pengji <jipengpro@gmail.com>
2021-04-25 17:49:50 +0800

    add 174

---
+++ dp/lc_174_dungeon-game.cpp
@@ -0,0 +1,21 @@
+/**
+ * 174. 地下城游戏
+ * https://leetcode-cn.com/problems/dungeon-game/
+ */
+
+class Solution {
+public:
+    // https://www.acwing.com/solution/content/4499/
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int m = dungeon.size(), n = dungeon[0].size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, INT_MAX));
+        dp[m][n-1] = 1, dp[m-1][n] = 1;
+        for(int i = m-1; i >= 0; i--) {
+            for(int j = n-1; j >= 0; j--) {
+                dp[i][j] = max(1, min(dp[i+1][j], dp[i][j+1]) - dungeon[i][j]);
+            }
+        }
+
+        return dp[0][0];
+    }
+};
