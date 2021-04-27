fdf26e5a5344c82412592193d36ab347058ebe89
pengji <jipengpro@gmail.com>
2021-04-27 22:19:39 +0800

    add 204

---
+++ math/lc_204_count-primes.cpp
@@ -0,0 +1,23 @@
+/**
+ * 204. 计数质数
+ * https://leetcode-cn.com/problems/count-primes/
+ */
+
+class Solution {
+public:
+    // https://www.acwing.com/solution/content/314/
+    int countPrimes(int n) {
+        vector<int> primes;
+        vector<bool> st(n + 1);
+        for (int i = 2; i < n; i ++ )
+        {
+            if (!st[i]) primes.push_back(i);
+            for (int j = 0; i * primes[j] < n; j ++ )
+            {
+                st[i * primes[j]] = true;
+                if (i % primes[j] == 0) break;
+            }
+        }
+        return primes.size();
+    }
+};
