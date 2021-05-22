b748649f37edd1cc41c9aa0729df741f3db4f441
pengji <jipengpro@gmail.com>
2021-05-22 20:46:38 +0800

    add 299

---
+++ hash/lc_299_bulls-and-cows.cpp
@@ -0,0 +1,26 @@
+/**
+ * 299. 猜数字游戏
+ * https://leetcode-cn.com/problems/bulls-and-cows/
+ */
+
+class Solution {
+public:
+    // https://www.acwing.com/activity/content/code/content/456565/
+    string getHint(string secret, string guess) {
+        unordered_map<char, int> hash;
+        for (auto c: secret) hash[c] ++ ;
+        int tot = 0;
+        for (auto c: guess)
+            if (hash[c]) {
+                tot ++ ;
+                hash[c] -- ;
+            }
+
+        int bulls = 0;
+        for (int i = 0; i < secret.size(); i ++ )
+            if (secret[i] == guess[i])
+                bulls ++ ;
+
+        return to_string(bulls) + "A" + to_string(tot - bulls) + "B";
+    }
+};
--- math/lc_292_nim-game.cpp
+++ math/lc_292_nim-game.cpp
@@ -6,6 +6,6 @@
 class Solution {
 public:
     bool canWinNim(int n) {
-
+        return n & 3;
     }
 };
