4286b29d15f3d1ac0138c0466cbf3a47bd317db6
pengji <jipengpro@gmail.com>
2021-07-17 08:48:04 +0800

    modify 42

--- methods/lc_42_trapping-rain-water.cpp
+++ methods/lc_42_trapping-rain-water.cpp
@@ -62,4 +62,23 @@ public:
 
         return res;
     }
+
+    // 利用栈
+    int trap(vector<int>& height) {
+        stack<int> stk;
+        int res = 0;
+        for (int i = 0; i < height.size(); i ++ ) {
+            int last = 0;
+            while (stk.size() && height[stk.top()] <= height[i]) {
+                res += (height[stk.top()] - last) * (i - stk.top() - 1);
+                last = height[stk.top()];
+                stk.pop();
+            }
+
+            if (stk.size()) res += (i - stk.top() - 1) * (height[i] - last);
+            stk.push(i);
+        }
+
+        return res;
+    }
 };
