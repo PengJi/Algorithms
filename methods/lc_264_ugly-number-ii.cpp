33677ba811fa38a4034c4b78b0314868155198ef
pengji <jipengpro@gmail.com>
2021-07-30 17:07:42 +0800

    modify 264

--- methods/lc_264_ugly-number-ii.cpp
+++ methods/lc_264_ugly-number-ii.cpp
@@ -18,4 +18,25 @@ public:
 
         return p.back();
     }
+
+    int nthUglyNumber(int n) {
+        vector<int> s(n);
+        s[0] = 1;
+        int i = 0, j = 0, k = 0;
+        int idx = 1;
+        int v2, v3, v5;
+        int m;
+        while(idx <= n-1) {
+            v2 = s[i] * 2;
+            v3 = s[j] * 3; 
+            v5 = s[k] * 5;
+            m = min(v2, min(v3, v5));
+            s[idx++] = m;
+            if(v2 == m) i++;
+            if(v3 == m) j++;
+            if(v5 == m) k++;
+        }
+
+        return s[n-1];
+    }
 };
