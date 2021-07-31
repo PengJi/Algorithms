e851c35656b24847097d5fb6832aff0372f078f4
pengji <jipengpro@gmail.com>
2021-07-31 17:36:20 +0800

    modify 283

--- methods/lc_283_move-zeroes.cpp
+++ methods/lc_283_move-zeroes.cpp
@@ -22,4 +22,12 @@ public:
             }
         }
     }
+
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        for(auto x : nums) {
+            if(x) nums[i++] = x;
+        }
+        while(i < nums.size()) nums[i++] = 0;
+    }
 };
