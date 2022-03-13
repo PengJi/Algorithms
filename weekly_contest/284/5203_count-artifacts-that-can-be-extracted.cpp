0befd4ed7bc7dc8d4edd8b48bafab24e01d6e0dc
pengji <jipengpro@gmail.com>
2022-03-13 16:38:44 +0800

    add 5203

---
+++ weekly_contest/284/5203_count-artifacts-that-can-be-extracted.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    typedef pair<int, int> pii;
+    
+    bool check(vector<int> &a, set<pii> &p) {
+        for(int i = a[0]; i <= a[2]; i++) {
+            for(int j = a[1]; j <= a[3]; j++) {
+                if(p.count(pii(i, j)) < 1) return false;
+            }
+        }
+
+        return true;
+    }
+
+    int digArtifacts(int n, vector<vector<int>>& artifacts, vector<vector<int>>& dig) {
+        int res = 0;
+        set<pii> st;
+        for(auto &d : dig) st.insert(pii(d[0], d[1]));
+        for(auto &a : artifacts) {
+            if(check(a, st)) res++;
+        }
+
+        return res;
+    }
+};
