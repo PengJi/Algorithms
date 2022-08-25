0629b4ccb43a507cf6a34fb08e150e665c6f1352
pengji <jipengpro@gmail.com>
2022-08-25 21:30:43 +0800

    modify 567

--- methods/lc_567_permutation-in-string.cpp
+++ methods/lc_567_permutation-in-string.cpp
@@ -45,3 +45,33 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    unordered_map<char, int> m1, m2;
+
+    bool check(char a) {
+        if(m1.count(a) && m1[a] == m2[a]) return true;
+        return false;
+    }
+
+    bool checkInclusion(string s1, string s2) {
+        for(auto& x : s1) m1[x]++;
+        for(int i = 0, j = 0, cnt = 0; i < s2.size(); i++) {
+            if(check(s2[i])) cnt--;
+            m2[s2[i]]++;
+            if(check(s2[i])) cnt++;
+
+            if(i - j >= s1.size()) {
+                if(check(s2[j])) cnt--;
+                m2[s2[j]]--;
+                if(check(s2[j])) cnt++;
+                j++;
+            }
+            if(cnt == m1.size()) return true;
+        }
+
+        return false;
+    }
+};
+
