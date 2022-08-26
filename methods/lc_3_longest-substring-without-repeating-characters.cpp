2cdcc668c33231412a4f4883c84b886bbc4aab0e
pengji <jipengpro@gmail.com>
2022-08-26 22:42:02 +0800

    modify 3

--- methods/lc_3_longest-substring-without-repeating-characters.cpp
+++ methods/lc_3_longest-substring-without-repeating-characters.cpp
@@ -58,3 +58,19 @@ public:
         return max;
     }
 };
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> mp;
+        int res = 0;
+        for(int i = 0, j = 0; i < s.size(); i++) {
+            mp[s[i]]++;
+            while(mp[s[i]] > 1) mp[s[j++]]--;
+            res = max(res, i - j + 1);
+        }
+
+        return res;
+    }
+};
+
