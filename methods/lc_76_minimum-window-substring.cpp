16f5fe7686073472527ecf2b60eacd97e419b0fd
pengji <jipengpro@gmail.com>
2022-09-03 21:35:40 +0800

    modify 76

--- methods/lc_76_minimum-window-substring.cpp
+++ methods/lc_76_minimum-window-substring.cpp
@@ -47,22 +47,24 @@ public:
         }
         return minLen == INT_MAX ? "" : s.substr(start, minLen);
     }
+};
 
+class Solution {
+public:
     string minWindow(string s, string t) {
-        unordered_map<char, int> h;
-        int cnt = 0;
-        for(auto c : t) {
-            if(!h[c]) cnt++;
-            h[c]++;
-        }
+        unordered_map<char, int> hs, ht;  // hs 表示窗口内各字符的个数，ht 表示目标字符串中各字符的个数
+        for(auto& c: t) ht[c]++;
 
-        string res = "";
-        for(int i=0, j=0, c=0; i < s.size(); i++) {
-            if(h[s[i]] == 1) c++;
-            h[s[i]]--;
-            while(c == cnt && h[s[j]] < 0) h[s[j++]]++;
-            if(c == cnt) {
-                if(res.empty() || res.size() > i - j + 1) res = s.substr(j, i - j + 1);
+        string res;
+        int cnt = 0;  // 窗口内有效字符的个数
+        for(int i = 0, j = 0; i < s.size(); i++) {
+            hs[s[i]]++;  // 窗口内对应字符的个数
+            if(hs[s[i]] <= ht[s[i]]) cnt++;  // 有效字符
+            while(hs[s[j]] > ht[s[j]]) hs[s[j++]]--; 
+            if(cnt == t.size()) {  // 有效字符个数等于目标字符串的字符个数
+                if(res.empty() || (i-j+1) < res.size()) {
+                    res = s.substr(j, i-j+1);
+                }
             }
         }
 
