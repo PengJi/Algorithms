229c3fad8b506b1c090b71050502d412d2e27f30
pengji <jipengpro@gmail.com>
2022-09-10 23:42:28 +0800

    modify 141

--- list/lc_141_linked-list-cycle.cpp
+++ list/lc_141_linked-list-cycle.cpp
@@ -29,3 +29,20 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head || !head->next) return false;
+        ListNode* s = head, *f = head->next;
+        while(f) {
+            s = s->next, f = f->next;
+            if(!f) return false;
+            f = f->next;
+            if(s == f) return true;
+        }
+
+        return false;
+    }
+};
+
