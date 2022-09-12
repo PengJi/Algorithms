fc7447039b87623e3e2c4ca48812669bbf86815d
pengji <jipengpro@gmail.com>
2022-09-12 22:58:43 +0800

    modify 107

--- tree/lc_107_binary-tree-level-order-traversal-ii.cpp
+++ tree/lc_107_binary-tree-level-order-traversal-ii.cpp
@@ -40,3 +40,28 @@ public:
         return max(depth(root->left),depth(root->right))+1;
     }
 };
+
+
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> res;
+        queue<TreeNode*> q;
+        if(root) q.push(root);
+        while(!q.empty()) {
+            int len = q.size();
+            vector<int> vec;
+            while(len--) {
+                TreeNode* node = q.front();
+                q.pop();
+                vec.push_back(node->val);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            res.push_back(vec);
+        }
+        reverse(res.begin(), res.end());
+
+        return res;
+    }
+};
