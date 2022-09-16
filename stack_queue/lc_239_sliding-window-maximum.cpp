3d2cfd0cd0c336f4df10d9813e59085cd62d55fa
pengji <jipengpro@gmail.com>
2022-09-16 23:11:09 +0800

    modify 239

--- stack_queue/lc_239_sliding-window-maximum.cpp
+++ stack_queue/lc_239_sliding-window-maximum.cpp
@@ -34,3 +34,19 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> res;
+        for(int i = 0; i < nums.size(); i++) {
+            if(dq.size() && i - k + 1 > dq.front()) dq.pop_front();
+            while(dq.size() && nums[i] > nums[dq.back()]) dq.pop_back();
+            dq.push_back(i);
+            if(i >= k-1) res.push_back(nums[dq.front()]);
+        }
+
+        return res;
+    }
+};
