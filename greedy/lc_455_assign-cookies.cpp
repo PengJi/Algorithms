11528b6d9d068d2e2e1453ee7ce1343707a689ed
pengji <jipengpro@gmail.com>
2022-09-20 22:35:12 +0800

    modify 455

--- greedy/lc_455_assign-cookies.cpp
+++ greedy/lc_455_assign-cookies.cpp
@@ -1,6 +1,5 @@
 /**
  * 455. 分发饼干
- * 
  * https://leetcode-cn.com/problems/assign-cookies/
  */
 
@@ -18,6 +17,26 @@ public:
                 i++;
             }
 
+            j++;
+        }
+
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int res = 0;
+        int i = 0, j = 0;
+        while(i < g.size() && j < s.size()) {
+            if(g[i] <= s[j]) {
+                res++;
+                i++;
+            }
             j++;
         }
 
