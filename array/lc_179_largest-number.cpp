78394050382256325564076c2a25eead57e05104
pengji <jipengpro@gmail.com>
2022-09-27 22:33:25 +0800

    modify 179

--- array/lc_179_largest-number.cpp
+++ array/lc_179_largest-number.cpp
@@ -19,3 +19,18 @@ public:
         return res.substr(k);
     }
 };
+
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), [](int x, int y) {
+            string a = to_string(x), b = to_string(y);
+            return a + b > b + a;
+        });
+        string res;
+        for (auto x: nums) res += to_string(x);
+        int k = 0;
+        while (k + 1 < res.size() && res[k] == '0') k ++ ;
+        return res.substr(k);
+    }
+};
