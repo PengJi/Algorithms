51982dcbf153865a6c79470bf468b2cc70541a76
pengji <jipengpro@gmail.com>
2022-09-29 23:16:16 +0800

    modify 57

--- array/lc_57_insert-interval.cpp
+++ array/lc_57_insert-interval.cpp
@@ -32,3 +32,33 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> res;
+        int left = newInterval[0], right = newInterval[1];
+        bool placed = false;  // 新区间是否被插入
+
+        for(auto x: intervals) {
+            if(x[1] < left) {  // 在插入区间的左侧
+                res.push_back(x);
+            } else if(x[0] > right) {  // 在插入区间的右侧
+                if(!placed) {  // 如果新区建没有被插入，则先插入新区间
+                    res.push_back({left, right});
+                    placed = true;
+                }
+                res.push_back(x);
+            } else {  // 区间重叠
+                left = min(left, x[0]);
+                right = max(right, x[1]);
+            }
+        }
+
+        if(!placed) {
+            res.push_back({left, right});
+        }
+
+        return res;
+    }
+};
