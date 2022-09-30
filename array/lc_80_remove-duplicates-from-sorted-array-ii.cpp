09b873c9739531ceb55aeedb8ddbe4004b5cc5a0
pengji <jipengpro@gmail.com>
2022-09-30 21:29:50 +0800

    modify 80

--- array/lc_80_remove-duplicates-from-sorted-array-ii.cpp
+++ array/lc_80_remove-duplicates-from-sorted-array-ii.cpp
@@ -19,3 +19,17 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 0;  // 有效索引
+        for(auto x: nums) {
+            if(k < 2 || nums[k-2] != x) {
+                nums[k++] = x;  // 有效数字增长
+            }
+        }
+
+        return k;
+    }
+};
