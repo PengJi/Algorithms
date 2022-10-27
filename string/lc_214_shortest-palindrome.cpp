1be97ce10bfb8a3d40ccb81756c173fed9f2a682
pengji <jipengpro@gmail.com>
2022-10-27 21:48:58 +0800

    add 214

--- string/lc_214_shortest-palindrome.cpp
+++ string/lc_214_shortest-palindrome.cpp
@@ -21,3 +21,34 @@ public:
         return string(right.rbegin(), right.rend()) + left + right;
     }
 };
+
+class Solution {
+public:
+    // kmp 算法，待深入
+    // 大体思路是求最长回文前缀，得到最长回文前缀后，便可得到得到需要添加的子字符串
+    // 将字符串逆序拼接到原字符串的末尾，然后使用 kmp 算法
+    string shortestPalindrome(string s) {
+        int n = s.size();
+
+        // 逆序拼接到原字符串的末尾
+        string t(s.rbegin(), s.rend());
+        string str(s + "#" + t);
+
+        // 新字符串
+        vector<int> nxt(2 * n + 1);
+
+        int j = -1;
+        nxt[0] = -1;
+
+        // kmp 算法
+        for (int i = 1; i < 2 * n + 1; i++) {
+            while (j > -1 && str[j + 1] != str[i]) j = nxt[j];
+            if (str[j + 1] == str[i])
+                j++;
+
+            nxt[i] = j;
+        }
+
+        return t.substr(0, n - nxt[2 * n] - 1) + s;
+    }
+};
