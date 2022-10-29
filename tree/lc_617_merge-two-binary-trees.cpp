c5db091a5c745c73eab4ef69d552cf096a120e11
pengji <jipengpro@gmail.com>
2022-10-29 22:50:03 +0800

    add 617

--- tree/lc_617_merge-two-binary-trees.cpp
+++ tree/lc_617_merge-two-binary-trees.cpp
@@ -29,3 +29,20 @@ public:
         return t1;
     }
 };
+
+class Solution {
+public:
+    // 以第一棵树为基准，将第二课树加到第一课树上
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if(!t1 && !t2) return NULL;
+        if(!t1 && t2) return t2;
+        if(t1 && !t2) return t1;
+
+        t1->val += t2->val;
+
+        t1->left = mergeTrees(t1->left, t2->left);
+        t1->right = mergeTrees(t1->right, t2->right);
+
+        return t1;
+    }
+};
