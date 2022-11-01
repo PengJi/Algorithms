6909ccd521806f953c9fda1fcfc707081d3e1d4e
pengji <jipengpro@gmail.com>
2022-11-01 23:35:04 +0800

    modify 300

--- dp/lc_300_longest-increasing-subsequence.cpp
+++ dp/lc_300_longest-increasing-subsequence.cpp
@@ -30,3 +30,28 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    // 状态表示：dp[i]记录以 nums[i] 结尾（即nums[i]为最后一个数字）的最长递增子序列的长度。
+    // 初始状态：dp[0] = 1
+    // 状态转移：dp[i]=max(dp[j]+1)，其中要求 1≤j<i 且 nums[j]<nums[i]。
+    // 时间复杂度为 O(n^2)，对于每个 i，都需要从 1 遍历到 i
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1, 1);
+        int res = 1;
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j]+1);
+                }
+            }
+
+            if(dp[i] > res) res = dp[i];
+        }
+
+        return res;
+    }
+};
