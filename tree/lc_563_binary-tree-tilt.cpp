6f40957a72eba38a7d9d100a5781b29b4e866c11
pengji <jipengpro@gmail.com>
2022-11-10 23:38:46 +0800

    modify 563

--- tree/lc_563_binary-tree-tilt.cpp
+++ tree/lc_563_binary-tree-tilt.cpp
@@ -38,3 +38,20 @@ public:
         return left + right + root->val;
     }
 };
+
+class Solution {
+public:
+    int res = 0;
+
+    int findTilt(TreeNode* root) {
+        dfs(root);
+        return res;
+    }
+
+    int dfs(TreeNode* root) {
+        if(!root) return 0;
+        int left = dfs(root->left), right = dfs(root->right);
+        res += abs(left - right);
+        return left + right + root->val;
+    }
+};
