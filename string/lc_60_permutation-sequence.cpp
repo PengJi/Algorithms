48981a57765d60e630c5aa0423618400adb91b97
pengji <jipengpro@gmail.com>
2022-11-11 23:47:45 +0800

    modify 60

--- string/lc_60_permutation-sequence.cpp
+++ string/lc_60_permutation-sequence.cpp
@@ -35,26 +35,30 @@ public:
 
 class Solution {
 public:
-    // 从小到大依次确定每一位
+    // 从高位到低位依次确定每一位
+    // 对于每一位，从小到大依次枚举未使用过的数，确定当前位
     string getPermutation(int n, int k) {
         string res;
 
         // 表示当前位是否用过
         vector<bool> vec(n);
 
+        // 从高位到低位依次确定每一位
         for(int i = 0; i < n; i++) {
-
+            // 求阶乘
             int f = 1;
             for(int j = 1; j < n - i; j++) f *= j;
 
+            // 从小到大枚举未使用过的数
             for(int j = 0; j < n; j++) {
                 if(!vec[j]) {  // 如果当前为没有被使用过
-                    if(k <= f) {  // 
+                    if(k <= f) {  // 可以确定一位
                         res += to_string(j+1);
                         vec[j] = true;
                         break;
                     }
-                    k -= f;
+
+                    k -= f;  // 剩余第几位
                 }
             }
         }
