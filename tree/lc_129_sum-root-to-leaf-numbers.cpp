0efe9ff319d5c5706f10e1084d8211892dfd302f
pengji <jipengpro@gmail.com>
2022-11-14 23:23:05 +0800

    modify 129

--- tree/lc_129_sum-root-to-leaf-numbers.cpp
+++ tree/lc_129_sum-root-to-leaf-numbers.cpp
@@ -34,3 +34,25 @@ public:
         if(!root->left && !root->right) ans += sum;
     }
 };
+
+class Solution {
+public:
+    int ans = 0;
+
+    int sumNumbers(TreeNode* root) {
+        dfs(root, 0);
+        return ans;
+    }
+
+    // 从根节点递归遍历整棵树，遍历时维护从根节点到该节点的路径表示的数，当遍历到叶节点时，将路径表示的数累加到答案中。
+    void dfs(TreeNode* n, int s) {
+        if (!n) return;
+
+        s = s * 10 + n->val;
+
+        if (n->left) dfs(n->left, s);
+        if (n->right) dfs(n->right, s);
+
+        if (!n->left && !n->right) ans += s;
+    }
+};
