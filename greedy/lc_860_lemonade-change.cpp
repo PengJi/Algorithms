91f931c7df4a07d3c5d6209f93b317b6cb242945
pengji <jipengpro@gmail.com>
2022-11-29 22:57:19 +0800

    modify 860

--- greedy/lc_860_lemonade-change.cpp
+++ greedy/lc_860_lemonade-change.cpp
@@ -30,3 +30,37 @@ public:
         return true;
     }
 };
+
+class Solution {
+public:
+    // 贪心算法
+    // 两个变量分别记录5元和10元的数量
+    // 收到5元，数量加一；
+    // 收到10元，如果没有5元，则返回 false；
+    // 收到20元，如果有10元，并且至少有一张5元，则 10+5；如果没有10元，则至少有3张5元，否则，返回false。
+    bool lemonadeChange(vector<int>& bills) {
+        int fives = 0, tens = 0;
+        for (auto &x : bills) {
+            if (x == 5)
+                fives++;
+            else if (x == 10) {
+                if (fives == 0)
+                    return false;
+                tens++;
+                fives--;
+            }
+            else {
+                if (tens >= 1 && fives >= 1) {
+                    tens--;
+                    fives--;
+                }
+                else if (tens == 0 && fives >= 3) {
+                    fives -= 3;
+                }
+                else
+                    return false;
+            }
+        }
+        return true;
+    }
+};
