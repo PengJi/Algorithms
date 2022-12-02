8f8b80cdb59e7185c34f39a8808a330476ac05e5
pengji <jipengpro@gmail.com>
2022-12-02 23:45:42 +0800

    modify 202

--- math/lc_202_happy-number.cpp
+++ math/lc_202_happy-number.cpp
@@ -30,3 +30,26 @@ public:
         return sum;
     }
 };
+
+class Solution {
+public:
+    int get(int x) {
+        int res = 0;
+        while (x) {
+            res += (x % 10) * (x % 10);
+            x /= 10;
+        }
+        return res;
+    }
+
+    // 快慢指针
+    bool isHappy(int n) {
+        int fast = get(n), slow = n;
+        while (fast != slow) {
+            fast = get(get(fast));
+            slow = get(slow);
+        }
+        return fast == 1;
+    }
+};
+
