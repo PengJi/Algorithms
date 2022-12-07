823237271c85bea82c3639826229dd894e39a7c2
pengji <jipengpro@gmail.com>
2022-12-07 22:10:10 +0800

    modify 13

--- math/lc_13_roman-to-integer.cpp
+++ math/lc_13_roman-to-integer.cpp
@@ -26,3 +26,29 @@ public:
        return sum;
     }
 };
+
+
+class Solution {
+public:
+    // 从后向前遍历
+    int romanToInt(string s) {
+        unordered_map<char, int> mp = { { 'I' , 1 },
+                                    { 'V' , 5 },
+                                    { 'X' , 10 },
+                                    { 'L' , 50 },
+                                    { 'C' , 100 },
+                                    { 'D' , 500 },
+                                    { 'M' , 1000 } };
+        int len = s.size();
+        int sum = mp[s[len-1]];
+        for(int i = len-2; i >= 0; i--) {
+            if(mp[s[i]] < mp[s[i+1]]) {
+                sum -= mp[s[i]];
+            } else {
+                sum += mp[s[i]];
+            }
+        }
+
+        return sum;    
+    }
+};
