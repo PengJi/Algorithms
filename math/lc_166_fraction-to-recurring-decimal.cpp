1960be6b0b536c0ca67f630753691263b905429e
pengji <jipengpro@gmail.com>
2022-12-09 00:10:12 +0800

    modify 166

--- math/lc_166_fraction-to-recurring-decimal.cpp
+++ math/lc_166_fraction-to-recurring-decimal.cpp
@@ -36,3 +36,44 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    // 模拟除法
+    // 时间复杂度为 O(n)
+    string fractionToDecimal(int numerator, int denominator) {
+        long long n = numerator, d = denominator;
+
+        // 判断正负数
+        bool minus = false;
+        if(n < 0) minus = !minus, n = -n;
+        if(d < 0) minus = !minus, d = -d;
+
+        string res = to_string(n / d);
+        n %= d;
+        if(!n) {  // 能够除尽，直接返回结果
+            if(minus && res != "0") return '-' + res;
+            return res;
+        }
+
+        // 除不尽则获取循环部分
+        res = res + '.';
+        unordered_map<int, int> mp;  // 判断循环部分，值为字符串下标
+        while(n) {  // 余数不等于0
+            if(mp[n]) {  // 找到了第一个循环的下标，返回结果
+                res = res.substr(0, mp[n]) + '(' + res.substr(mp[n]) + ')';
+                break;
+            } else {
+                mp[n] = res.size();  // 添加下标
+            }
+
+            n *= 10;  // 有余数则向后除
+            res += to_string(n / d);  // 更新字符串
+            n %= d;  // 求余数
+        }
+
+        if(minus) res = '-' + res;
+        return res;
+    }
+};
