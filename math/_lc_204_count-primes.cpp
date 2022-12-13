28965a24d2e3b24d88558e5cf0a6eb6f7f5768d0
pengji <jipengpro@gmail.com>
2022-12-13 23:21:23 +0800

    modify 204

--- math/_lc_204_count-primes.cpp
+++ math/_lc_204_count-primes.cpp
@@ -21,3 +21,24 @@ public:
         return primes.size();
     }
 };
+
+class Solution {
+public:
+    // 最优的算法
+    // 时间复杂度：O(n)
+    int countPrimes(int n) {
+        vector<int> primes;
+        vector<bool> st(n + 1);
+
+        for (int i = 2; i < n; i ++ ) {
+            if (!st[i]) primes.push_back(i);
+
+            for (int j = 0; i * primes[j] < n; j ++ ) {
+                st[i * primes[j]] = true;
+                if (i % primes[j] == 0) break;
+            }
+        }
+
+        return primes.size();
+    }
+};
