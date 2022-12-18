e29fda74c15f95458359c046733d99ddfaef83e0
pengji <jipengpro@gmail.com>
2022-12-18 23:24:06 +0800

    modify 268

--- math/lc_268_missing-number.cpp
+++ math/lc_268_missing-number.cpp
@@ -12,3 +12,14 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    // 对数组的数字求和，然后求差值
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int res = n * (n+1) / 2;
+        for(auto x: nums) res -= x;
+        return res;
+    }
+};
