ec3b0893a0f4b9bbfd74f8f96893c960c556f218
pengji <jipengpro@gmail.com>
2022-12-30 15:19:11 +0800

    modify 90

--- backtrack/lc_90_subsets-ii.cpp
+++ backtrack/lc_90_subsets-ii.cpp
@@ -1,6 +1,7 @@
 /**
  * 90. 子集 II
  * 给定一个可能包含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ * 解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
  * 
  * https://leetcode-cn.com/problems/subsets-ii/
  */
@@ -26,6 +27,37 @@ public:
             while(i < nums.size()-1 && nums[i] == nums[i+1]){
                 i++;
             }
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        dfs(nums, 0);
+        return res;
+    }
+
+    void dfs(vector<int>& nums, int idx) {
+        if(idx == nums.size()) {
+            res.push_back(path);
+            return;
+        }
+
+        int k = idx + 1;
+        while(k < nums.size() && nums[k] == nums[idx]) k++;
+
+        for(int i = 0; i <= k - idx; i++) {
+            dfs(nums, k);
+            path.push_back(nums[idx]);
+        }
+
+        for(int i = 0; i <= k - idx; i++) {
+            path.pop_back();
         }
     }
 };
