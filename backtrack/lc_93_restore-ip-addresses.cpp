bc6d46cef0b27c98c3fd649deb1d9c71fe142de8
pengji <jipengpro@gmail.com>
2022-12-31 15:52:18 +0800

    modify 93

--- backtrack/lc_93_restore-ip-addresses.cpp
+++ backtrack/lc_93_restore-ip-addresses.cpp
@@ -44,3 +44,40 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    vector<string> ans;
+    vector<int> path;
+
+    vector<string> restoreIpAddresses(string s) {
+        dfs(0, 0, s);
+        return ans;
+    }
+
+    // idx 表示枚举到的字符串下标，k表示当前截断的IP个数，s表示原字符串
+    void dfs(int idx, int k, string &s) {
+        if (idx == s.size()) {
+            if (k == 4) {
+                string ip = to_string(path[0]);
+                for(int i = 1; i < 4; i ++ )
+                    ip += '.' + to_string(path[i]);
+                ans.push_back(ip);
+            }
+            return;
+        }
+        if (k > 4) return;  // 剪枝
+
+        unsigned t = 0;
+        for (int i = idx; i < s.size(); i ++ ) {
+            t = t * 10 + s[i] - '0';
+            if (t >= 0 && t < 256) {
+                path.push_back(t);
+                dfs(i + 1, k + 1, s);
+                path.pop_back();
+            }
+            
+            if (!t) break;
+        }
+    }
+};
