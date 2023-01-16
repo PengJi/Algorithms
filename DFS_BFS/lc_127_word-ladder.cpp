613a1d2f27278c7b4e55cfb12746454efebd69d8
pengji <jipengpro@gmail.com>
2023-01-16 23:28:13 +0800

    modify 127

--- DFS_BFS/lc_127_word-ladder.cpp
+++ DFS_BFS/lc_127_word-ladder.cpp
@@ -45,3 +45,37 @@ public:
         return 0; 
     }
 };
+
+class Solution {
+public:
+    // 将单词看做点，如果两个单词可以相互转化，则在相应的点之间连一条无向边。那问题就变成了求从起点到终点的最短路。
+    // 枚举每个单词，然后枚举该单词的每一位字母，再枚举这一位的所有备选字母，然后再判断改变后的字符串是否存在。
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> st;
+        for(auto& s: wordList) st.insert(s);
+        unordered_map<string, int> mp;
+        mp[beginWord] = 0;
+        queue<string> q;
+        q.push(beginWord);
+
+        while(q.size()) {
+            string t = q.front();
+            q.pop();
+
+            string r = t;
+            for(int i = 0; i < t.size(); i++) {
+                t = r;
+                for(char j = 'a'; j <= 'z'; j++) {
+                    t[i] = j;
+                    if(st.count(t) && !mp.count(t)) {
+                        mp[t] = mp[r] + 1;
+                        if(t == endWord) return mp[t] + 1;
+                        q.push(t);
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+};
