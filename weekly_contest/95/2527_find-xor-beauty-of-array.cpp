c79a4fe8315edbfdaf603d46f02654b5dda281b0
pengji <jipengpro@gmail.com>
2023-03-06 19:02:27 +0800

    add 2527

---
+++ weekly_contest/95/2527_find-xor-beauty-of-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int xorBeauty(vector<int>& nums) {
+        int res = 0;
+        for(auto& n : nums) {
+            res ^= n;
+        }
+
+        return res;
+    }
+};
