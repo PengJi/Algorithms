8ffd0973d92047f4b43488dba27835d5d67adedb
pengji <jipengpro@gmail.com>
2023-04-07 09:04:40 +0800

    add 2562

---
+++ weekly_contest/332/2562_find-the-array-concatenation-value.cpp
@@ -0,0 +1,28 @@
+#define LL long long
+
+class Solution {
+public:
+    // 双指针
+    LL findTheArrayConcVal(vector<int>& nums) {
+        const int n = nums.size();
+        LL ans = 0;
+
+        for (int i = 0, j = n - 1; i <= j; i++, j--) {
+            if (i == j) {
+                ans += nums[i];
+
+                continue;
+            }
+
+            int x = nums[i], y = nums[j];
+            ans += y;
+            while (y) {
+                x *= 10;
+                y /= 10;
+            }
+            ans += x;
+        }
+
+        return ans;
+    }
+};
