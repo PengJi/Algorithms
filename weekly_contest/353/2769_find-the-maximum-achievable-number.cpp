7c6617d19e8bd69efa9916f0aa73d46462985453
pengji <jipengpro@gmail.com>
2023-09-15 23:01:12 +0800

    add 2769

---
+++ weekly_contest/353/2769_find-the-maximum-achievable-number.cpp
@@ -0,0 +1,11 @@
+/**
+ * 2769. 找出最大的可达成数字
+ * https://leetcode.cn/problems/find-the-maximum-achievable-number/description/
+*/
+
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        return num + 2 * t;
+    }
+};
