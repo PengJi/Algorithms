a8933a2ff0aecdbd74629656d72a8c0bc363f5e1
pengji <jipengpro@gmail.com>
2023-11-06 09:06:16 +0800

    add 2835

---
+++ weekly_contest/360/2835_minimum-operations-to-form-subsequence-with-target-sum.cpp
@@ -0,0 +1,27 @@
+/**
+ * 2835. 使子序列的和等于目标的最少操作次数
+ * https://leetcode.cn/problems/minimum-operations-to-form-subsequence-with-target-sum/description/
+*/
+class Solution {
+public:
+    // 贪心
+    int minOperations(vector<int> &nums, int target) {
+        if (accumulate(nums.begin(), nums.end(), 0LL) < target)
+            return -1;
+        long long cnt[31]{};
+        for (int x: nums)
+            cnt[__builtin_ctz(x)]++;
+        int ans = 0, i = 0;
+        long long s = 0;
+        while ((1LL << i) <= target) {
+            s += cnt[i] << i;
+            int mask = (1LL << ++i) - 1;
+            if (s >= (target & mask))
+                continue;
+            ans++; // 一定要找更大的数操作
+            for (; cnt[i] == 0; i++)
+                ans++; // 还没找到，继续找更大的数
+        }
+        return ans;
+    }
+};
