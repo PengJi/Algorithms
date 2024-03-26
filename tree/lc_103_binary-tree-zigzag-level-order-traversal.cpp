2501d7db1994e2eb56d78182372fcf2cf7d32ba3
pengji <jipengpro@gmail.com>
2024-03-27 07:54:42 +0800

    modify 103

--- tree/lc_103_binary-tree-zigzag-level-order-traversal.cpp
+++ tree/lc_103_binary-tree-zigzag-level-order-traversal.cpp
@@ -19,9 +19,7 @@ public:
     // https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/solution/er-cha-shu-de-ju-chi-xing-ceng-xu-bian-l-qsun/
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> ans;
-        if (!root) {
-            return ans;
-        }
+        if (!root) return ans;
 
         queue<TreeNode*> nodeQueue;
         nodeQueue.push(root);
@@ -30,6 +28,7 @@ public:
         while (!nodeQueue.empty()) {
             deque<int> levelList;
             int size = nodeQueue.size();
+
             for (int i = 0; i < size; ++i) {
                 auto node = nodeQueue.front();
                 nodeQueue.pop();
@@ -38,17 +37,48 @@ public:
                 } else {
                     levelList.push_front(node->val);
                 }
-                if (node->left) {
-                    nodeQueue.push(node->left);
-                }
-                if (node->right) {
-                    nodeQueue.push(node->right);
-                }
+                if (node->left) nodeQueue.push(node->left);
+                if (node->right) nodeQueue.push(node->right);
             }
             ans.emplace_back(vector<int>{levelList.begin(), levelList.end()});
             isOrderLeft = !isOrderLeft;
         }
 
         return ans;
+    }
+};
+
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        queue<TreeNode*> q;
+        if (root) q.push(root);
+        bool rev = false;
+
+        while (!q.empty()) {
+            int len = q.size();
+            vector<int> vec;
+
+            while (len--) {
+                TreeNode* cur = q.front();
+                q.pop();
+                vec.push_back(cur->val);
+                if (cur->left) q.push(cur->left);
+                if (cur->right) q.push(cur->right);
+            }
+
+            if (rev) {
+                reverse(vec.begin(), vec.end());
+                rev = false;
+            } else {
+                rev = true;
+            }
+
+            res.push_back(vec);
+        }
+
+        return res;
     }
 };
