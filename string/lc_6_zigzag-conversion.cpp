288f2f9bdaccf733ad6511d64292f83457cc6150
pengji <jipengpro@gmail.com>
2024-04-01 09:24:28 +0800

    modify string file

--- string/lc_6_zigzag-conversion.cpp
+++ string/lc_6_zigzag-conversion.cpp
@@ -1,14 +1,12 @@
 /**
  * 6. Z 字形变换
- * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
- * 
  * https://leetcode-cn.com/problems/zigzag-conversion/
  */
 
 class Solution {
 public:
     string convert(string s, int numRows) {
-        if(numRows == 1) return s;
+        if (numRows == 1) return s;
 
         string res;
         int n = s.size();
@@ -21,6 +19,33 @@ public:
                     res += s[j + cycleLen - i];
             }
         }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1) return s;
+
+        int offset = 2 * (numRows - 1);
+        string res;
+
+        for (int i = 0; i < numRows; i++) {    // 遍历每一行
+            if (i == 0 || i == numRows - 1) {  // 第一行和最后一行
+                for (int j = i; j < s.size(); j += offset) {
+                    res += s[j];
+                }
+            } else {  // 其余行
+                for (int m = i, n = numRows * 2 - 1 - i - 1;
+                     m < s.size() || n < s.size();
+                     m += offset, n += offset) {
+                    if (m < s.size()) res += s[m];
+                    if (n < s.size()) res += s[n];
+                }
+            }
+        }
+
         return res;
     }
 };
