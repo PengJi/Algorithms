2813ea6f903ecc5d21da829aaaa892c96b20c587
pengji <jipengpro@gmail.com>
2024-04-05 12:06:53 +0800

    modify string notes

--- string/_lc_30_substring-with-concatenation-of-all-words.cpp
+++ string/_lc_30_substring-with-concatenation-of-all-words.cpp
@@ -7,39 +7,38 @@ class Solution {
 public:
     // 滑动窗口
     vector<int> findSubstring(string s, vector<string>& words) {
-        vector<int>re;
-        if (s.empty() || words.empty()) 
+        vector<int> re;
+        if (s.empty() || words.empty())
             return re;
         int n = words[0].size(), length1 = s.size(), length2 = words.size();
         map<string, int> aa;
-        for (auto x : words) 
+        for (auto x : words)
             ++aa[x];
 
-        for (int i = 0; i < n; ++i){
-            int l = i, r = i;//  l指向滑动窗口最左边的单词的起始点， r指向滑动窗口最右边的单词的起始点
+        for (int i = 0; i < n; ++i) {
+            int l = i, r = i;  // l指向滑动窗口最左边的单词的起始点， r指向滑动窗口最右边的单词的起始点
             map<string, int> bb;
-            while (r + n <= s.size()){
-                if (aa.count(s.substr(r, n))){//有效单词
+            while (r + n <= s.size()) {
+                if (aa.count(s.substr(r, n))) {  //有效单词
                     string wd = s.substr(r, n);
                     ++bb[wd];
                     r += n;
-                    if (bb[wd] < aa[wd]) continue; // 当前单词个数小于目标单词个数，r右移，添加最右端单词(continue,跳到下一次循环自动执行)
+                    if (bb[wd] < aa[wd]) continue;  // 当前单词个数小于目标单词个数，r右移，添加最右端单词(continue,跳到下一次循环自动执行)
 
-                    while (bb[wd] > aa[wd]){   //当前单词个数大于目标单词个数，删除最左端单词，l右移
+                    while (bb[wd] > aa[wd]) {  //当前单词个数大于目标单词个数，删除最左端单词，l右移
                         if (--bb[s.substr(l, n)] == 0)
                             bb.erase(s.substr(l, n));
                         l += n;
-                    }//这里一定要注意用while循环（而不是if），直到当前单词个数等于目标单词个数，具体原因可以自己跑样例写下体会下  
-                    
-                    if (bb[wd] == aa[wd] && r - l == length2 * n){  //当前单词个数等于目标单词个数，比较目前单词总数与目标单词总数是否相等，
+                    }  //这里一定要注意用while循环（而不是if），直到当前单词个数等于目标单词个数，具体原因可以自己跑样例写下体会下
+
+                    if (bb[wd] == aa[wd] && r - l == length2 * n) {  //当前单词个数等于目标单词个数，比较目前单词总数与目标单词总数是否相等，
                         //如果不相等：r右移，添加最右端单词(跳到下一次循环自动执行)。如果相等：删除最左端单词，l右移；r右移，添加最右端单词(跳到下一次循环自动执行)。
                         re.push_back(l);
                         if (--bb[s.substr(l, n)] == 0)
                             bb.erase(s.substr(l, n));
                         l += n;
                     }
-                }
-                else {  //如果单词无效，则l,r跳到下一个单词处重新开始计数
+                } else {  //如果单词无效，则l,r跳到下一个单词处重新开始计数
                     bb.clear();
                     r += n;
                     l = r;
--- string/lc_43_multiply-strings.cpp
+++ string/lc_43_multiply-strings.cpp
@@ -9,29 +9,30 @@ public:
     // 时间复杂度：O(mn)
     // 空间复杂度：O(m+n)
     string multiply(string num1, string num2) {
-        if(num1 == "0" || num2 == "0") return "0";
+        if (num1 == "0" || num2 == "0") return "0";
 
         int len1 = num1.size(), len2 = num2.size();
         vector<int> res(len1 + len2);  // 将每位的结果放入数组
         int n1, n2, sum;
+
         // 从个位开始逐位相乘
-        for(int i = len1 - 1; i >= 0; i--) {
+        for (int i = len1 - 1; i >= 0; i--) {
             n1 = num1[i] - '0';
-            for(int j = len2 - 1; j >= 0; j--) {
+            for (int j = len2 - 1; j >= 0; j--) {
                 n2 = num2[j] - '0';
-                sum = res[i+j+1] + n1 * n2;
-                res[i+j+1] = sum % 10;  // 确定每位的值
-                res[i+j] += sum / 10;  //进位
+                sum = res[i + j + 1] + n1 * n2;
+                res[i + j + 1] = sum % 10;  // 确定每位的值
+                res[i + j] += sum / 10;     // 进位
             }
         }
 
         string ans = "";
         // 转为字符串
-        for(int i = 0; j < res.size(); i++) {
-            if(i == 0 && res[i] == 0) continue;  // 首位为0
-            ans += to_string(res[i]); 
+        for (int i = 0; i < res.size(); i++) {
+            if (i == 0 && res[i] == 0) continue;  // 首位为0
+            ans += to_string(res[i]);
         }
 
         return ans;
-    } 
+    }
 };
