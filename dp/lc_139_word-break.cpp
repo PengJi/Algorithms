3bdf0ba23ae5c6d050b2353df760d56fde933904
pengji <jipengpro@gmail.com>
2024-05-13 19:18:39 +0800

    modify dp notes

--- dp/lc_139_word-break.cpp
+++ dp/lc_139_word-break.cpp
@@ -1,35 +1,66 @@
 /**
  * 139. 单词拆分
- * 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，
- * 判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
- *
- * https://leetcode-cn.com/problems/word-break/
+ * https://leetcode.cn/problems/word-break/description/
  */
 
 class Solution {
 public:
-    // 动态规划
-    // https://leetcode-cn.com/problems/word-break/solution/dan-ci-chai-fen-by-leetcode-solution/
-    // 时间复杂度：O(n^2)
-    // 空间复杂度：O(n)
+    // https://leetcode.cn/problems/word-break/solutions/302471/dan-ci-chai-fen-by-leetcode-solution/
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> dict;
-        for (auto item : wordDict) {
-            dict.insert(item);
-        }
-
-        // dp[i] 表示字符串 s 前 i 个字符组成的字符串 s[0..i-1] 是否能被空格拆分成若干个字典中出现的单词
-        vector<bool> dp(s.size() + 1, false);
+        unordered_set<string> st;
+        for (auto& w : wordDict) st.insert(w);
+        int n = s.size();
+        vector<bool> dp(n + 1);  // dp[i] 表示字符串 sss 前 iii 个字符组成的字符串 s[0..i−1] 是否能被空格拆分成若干个字典中出现的单词
         dp[0] = true;
-        for (int i = 1; i <= s.size(); i++) {
+
+        for (int i = 0; i <= n; i++) {
             for (int j = 0; j < i; j++) {
-                if (dp[j] && dict.find(s.substr(j, i - j)) != dict.end()) {
+                if (dp[j] && st.count(s.substr(j, i - j))) {  // substr 需要优化
                     dp[i] = true;
                     break;
                 }
             }
         }
 
-        return dp[s.size()];
+        return dp[n];
+    }
+};
+
+class Solution {
+public:
+    typedef unsigned long long ULL;
+
+    // 算法同上，使用字符串哈希优化 substr
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<ULL> st;
+        const int p = 131;
+        int n = s.size();
+        vector<bool> dp(n + 1);
+        dp[0] = true;
+
+        // 将字符串转为整数存入哈希表
+        for (auto& word : wordDict) {
+            ULL key = 0;
+            for (auto& c : word) {
+                key = key * p + c;
+            }
+            st.insert(key);
+        }
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j]) {  // 通过字符串哈希表判断
+                    ULL key = 0;
+                    for (int k = j; k < i; k++) key = key * p + s[k];
+
+                    if (st.count(key)) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return dp[n];
     }
 };
--- dp/lc_53_maximum-subarray.cpp
+++ dp/lc_53_maximum-subarray.cpp
@@ -1,6 +1,6 @@
 /**
- * 53. 最大子序和
- * https://leetcode-cn.com/problems/maximum-subarray/
+ * 53. 最大子数组和
+ * https://leetcode.cn/problems/maximum-subarray/
  */
 
 class Solution {
